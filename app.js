da1d9a1dc4d9783d1e238895fb55f80544101eb2
zemrwantstolearncoding <surajitb75@gmail.com>
2024-06-23 00:08:29 +0530

    zemr's homepage

---
+++ app.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hidden2Elements = document.querySelectorAll('.hidden2');
+hidden2Elements.forEach((el) => observer.observe(el));
---
+++ specification.txt
@@ -0,0 +1,4 @@
+HTML tags used: nav, div, a, button, span, main, img, h5, p, footer
+CSS properties used: height, width, display, background-size, flex-direction
+JavaScript: used JavaScript to animate the contents of the page on load and on scroll
+Bootstrap: took inspiration from Bootstrap templates to make the navbar and the footer and used multiple Bootstrap CSS classes
